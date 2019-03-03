6aff16f8d0770757cea27f524a5c22455dbe7793
rk303hotmail <rk303@hotmail.com>
2019-03-03 17:56:04 -0500

    Initial Commit

--- Assignment1_S19/Program.cs
+++ Assignment1_S19/Program.cs
@@ -19,22 +19,24 @@ namespace Assignment1_S19
             Console.WriteLine("\n" + "Print Prime Numbers From " + x + " To " + y);
             printPrimeNumbers(x, y); /* calling printPrimeNumbers where starting range x is 5 and ending rante value of y is 25 */
 
-            // This is used to to check FindFactorial of each number provided in the assignment i.e. 2, 3 and 4 
+            // Codes are Used to to check FindFactorial of each number provided in the assignment i.e. 2, 3 and 4 
             Console.WriteLine("\n" + "Calculate Factorial");
             Console.WriteLine("Factorial of " + 2 + " is " + FindFactorial(2));
             Console.WriteLine("Factorial of " + 3 + " is " + FindFactorial(3));
             Console.WriteLine("Factorial of " + 4 + " is " + FindFactorial(4));
-                        
+            // Using method FindFactorial to computes the series 1/2 – 2!/3 + 3!/4 – 4!/5            
             Decimal dSeriesFact = (1/2) - (FindFactorial(2) / 3) + (FindFactorial(3) / 4) - (FindFactorial(4)) / 5;
             Console.WriteLine("Computed series of 1/2 – 2!/3 + 3!/4 – 4!/5 result is : " + dSeriesFact);
 
 
+            // DrawTriangle method calls other methods to draw a Triangle using *
             x = 1;
             y = 15;
             Console.WriteLine("\n" + "Draw Triangle using Odd Numbers from " + x + " To " + y);
             drawTriangle(x, y); /* Draw a Triangle of odd numbers starting 1*/
             Console.WriteLine();
 
+            // Count frequency of an element in the array and prints the result.
             Console.WriteLine("\n" + "Print Frequency of a number in Array" + "\n" + "\n" + "Number" + "\t" + "Frequency");
             
             for (int i = 1; i <= 3; i++)
@@ -90,7 +92,7 @@ namespace Assignment1_S19
             }           
         } /* end IsPrime */
 
-        public static int FindFactorial(int n)
+        public static int FindFactorial(int n) // Find Factorial of input parameter n
         {
             try
             {
@@ -109,7 +111,7 @@ namespace Assignment1_S19
             return 0;
         } // FindFactorial
 
-        public static Boolean IsOdd(int n)
+        public static Boolean IsOdd(int n) // Validate if n is an Odd Number and Returns True/False
         {
             try
             {
@@ -122,7 +124,7 @@ namespace Assignment1_S19
             }            
         } // end IsOdd
 
-        public static void drawTriangle(int x, int y)
+        public static void drawTriangle(int x, int y) // Draw Triangle, printSpace method ensures the alignment 
         {
             try
             {
@@ -143,7 +145,7 @@ namespace Assignment1_S19
             }            
         } // end drawTriangle
         
-        public static void printTriangle(int n)
+        public static void printTriangle(int n) // Prints Triangle using *
         {
             try
             {
@@ -158,7 +160,7 @@ namespace Assignment1_S19
             }            
         } // end printTriangle
 
-        public static void printSpace(int n)
+        public static void printSpace(int n) // Method is used by drawTriangle to ensure alignment
         {
             try
             {
@@ -173,7 +175,7 @@ namespace Assignment1_S19
             }
         } // end printSpace
 
-        public static int ComputeFrequency(int []a, int an, int x)
+        public static int ComputeFrequency(int []a, int an, int x)  // calculates frequency of a number from an array and returns the count
         {
             try
             {
